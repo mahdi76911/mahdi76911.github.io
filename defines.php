2594ade779883f9e9590229a763c16476627fb1d
Mohammad Mahdi Bideh <mahdi.76911@gmail.com>
2024-01-22 00:19:31 +0330

    static

--- defines.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define( 'INSTAGRAM_APP_ID', '1414294992822954' );
-	define( 'INSTAGRAM_APP_SECRET', '106e90c30c77792dde84ff23f381d45e' );
-	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://mahdi76911.github.io/' );
